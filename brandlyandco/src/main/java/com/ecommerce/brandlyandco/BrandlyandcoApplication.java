ec8da1d6d980464e932de272e86921ccfeb64cd6
MD. SANAULLAH <158471899+mdsanaullah2030@users.noreply.github.com>
2025-05-20 18:10:53 +0600

    warranty

---
+++ brandlyandco/src/main/java/com/ecommerce/brandlyandco/BrandlyandcoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.brandlyandco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandlyandcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandlyandcoApplication.class, args);
+	}
+
+}
---
+++ brandlyandco/src/test/java/com/ecommerce/brandlyandco/BrandlyandcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.brandlyandco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandlyandcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
